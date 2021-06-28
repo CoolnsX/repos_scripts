74e3ca2bd8f7ef15f487de568a248aec7f6dd563
CoolnsX <ansari.tan20@gmail.com>
2021-06-28 12:02:16 +0530

    update

--- st-0.8.4/config.h
+++ st-0.8.4/config.h
@@ -5,12 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-// static char *font = "Ubuntu Mono:style=Bold:pixelsize=16:antialias=true:autohint=true";
-/* Spare fonts */
-static char *font2[] = {
-    "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=tr    ue",
-    "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true"
-};
+ static char *font = "JetBrains Mono:style=Bold:pixelsize=14:antialias=true:autohint=true";
 
 static int borderpx = 0;
 
@@ -32,8 +27,8 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.0;
-static float chscale = 1.0;
+static float cwscale = 0.9;
+static float chscale = 0.9;
 
 /*
  * word delimiter string
