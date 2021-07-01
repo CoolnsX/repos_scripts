e8ce1a17342c617ce16b60eb3dbf756c69c04b86
CoolnsX <ansari.tan20@gmail.com>
2021-07-01 10:32:05 +0530

    update

--- st-0.8.4/config.h
+++ st-0.8.4/config.h
@@ -132,7 +132,7 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
+unsigned int defaultfg = 15;
 unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
