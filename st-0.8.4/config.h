56a6f29c094484566f756a67e550dd8fc72ab496
CoolnsX <ansari.tan20@gmail.com>
2021-07-15 08:41:29 +0530

    hehe

--- st-0.8.4/config.h
+++ st-0.8.4/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
- static char *font = "JetBrains Mono:style=Bold:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:style=Bold:pixelsize=14:antialias=true:autohint=true";
 
 static int borderpx = 0;
 
