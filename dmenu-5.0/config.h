6669adb3b8f7a8bc1455730cb43987c6bd822291
CoolnsX <ansari.tan20@gmail.com>
2021-08-23 17:03:15 +0530

    lol

--- dmenu-5.0/config.h
+++ dmenu-5.0/config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#5294e2" },
+	[SchemeSel] = { "#eeeeee", "#8800aa" },
 	[SchemeOut] = { "#000000", "#C000FF" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
