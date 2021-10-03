8275284136a9ee0b9ce8ff2f704c40df79463b94
CoolnsX <ansari.tan20@gmail.com>
2021-10-03 22:18:15 +0530

    remove ani-cli

--- st-0.8.4/config.def.h
+++ st-0.8.4/config.def.h
@@ -125,6 +125,7 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
 	"black",
+	"#FF9F12",
 };
 
 
@@ -132,7 +133,7 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 15;
+unsigned int defaultfg = 259;
 unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
