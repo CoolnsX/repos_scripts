94e91f3cf598cae6c9168a68e0b23848d758bd02
CoolnsX <ansari.tan20@gmail.com>
2022-08-11 01:22:11 +0530

    dwwidww

--- st-0.8.4/config.def.h
+++ st-0.8.4/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono NL:style=Regular:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrains Mono NL:style=Regular:pixelsize=15:antialias=true:autohint=true";
 
 static int borderpx = 0;
 
@@ -27,8 +27,8 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 0.9;
-static float chscale = 0.9;
+static float cwscale = 1.0;
+static float chscale = 1.0;
 
 /*
  * word delimiter string
